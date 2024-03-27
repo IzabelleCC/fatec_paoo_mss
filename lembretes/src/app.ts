4a9a2bcefe84fa457537008db95a01cde5d9897d
Izabelle Cesario <izabellecesario@hotmail.com>
2024-03-27 18:31:10 -0300

    feat(ap01-mss): implementa endpoints post e get de lembretes

---
+++ lembretes/src/app.ts
@@ -0,0 +1,70 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+/*
+    {
+        "1":{
+            "id":"1",
+            "texto": "Fazer cafe"
+        },
+        "2":{
+            "id":"2",
+            "texto": "Nadar"
+        }
+    }
+*/
+interface Lembrete{
+    id: string;
+    texto: string;
+}
+const lembretes: Record<string, Lembrete> = {}
+let id: string = '1'
+//GET /lembretes obter a coleção de lembretes
+app.get('/lembretes', (req,res) => {
+    res.json(lembretes)
+})
+
+//POST /lembretes cadastrar novo lembrete
+app.post('/lembretes', (req,res) => {
+    //extrair o texto do corpo da requisição
+    const { texto } = req.body
+    //construir um novo lembrete
+    const lembrete = { id, texto }
+    //armazenar o novo lembrete
+    lembretes[id] = lembrete
+    //incremento o id
+    id = (+id + 1).toString()
+    //responder ao cliente
+    res.json(lembrete)
+})
+
+//GET /lembretes/{id} obter um lembrete pelo id
+
+app.get('/lembretes/:id', (req,res) => {
+    const { id } = req.params;
+    const lembrete = lembretes[id];
+    if (lembrete) {
+        res.json(lembrete);
+    } else {
+        res.status(404).send('Lembrete nao encontrado');
+    }
+})
+
+app.put('/lembretes/:id', (req,res) => {
+    const { id } = req.params;
+    const lembrete = lembretes[id];
+    if (lembrete) {
+         //extrair o texto do corpo da requisição
+        const { texto } = req.body
+        //construir um novo lembrete
+        const lembrete = { id, texto }
+        //armazenar o novo lembrete
+        lembretes[id] = lembrete
+        res.json(lembrete);
+    } else {
+        res.status(404).send('Lembrete nao encontrado');
+    }
+})
+
+const port = 4000
+app.listen(port,() => console.log(`Lembretes. Porta ${port}.`))
